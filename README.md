6be31c32d561eddef93e85897ab5a2d276eb0cd9
Benjamin Goh <bengoh@paypal.com>
2014-05-11 07:47:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dustr
+
+There are lots of Dust npm modules, but I just wanted one that would take a template and model and give me an output.
+
+NOTE: This is a promises-based module.
+
+Some notes:
+
+* This will not automatically compress whitespace.
---
+++ test/test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var chai = require('chai'),
+	chaiAsPromised = require('chai-as-promised'),
+	dustr = require('../lib/dustr'),
+	fs = require('fs');
+
+chai.use(chaiAsPromised);
+var should = chai.should();
+
+describe('Dustr', function() {
+	it('should return an object with the render function', function() {
+		dustr().should.have.property('render');
+	});
+
+	describe('#render()', function() {
+		it('should be able to render a simple dust template', function() {
+			return dustr().render('Hello {name}, welcome to the world!', { name: 'Benjamin' })
+				.should.eventually.equal('Hello Benjamin, welcome to the world!');
+		});
+	});
+});
